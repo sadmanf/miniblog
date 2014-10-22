e87f4bdab990dd12334b34aedae5aebb3a933b97
Benedict-Bolton <benedict.bolton@gmail.com>
2014-10-22 12:14:41 -0400

    I modularized Sadman's working code

--- flaskr/app.py
+++ flaskr/app.py
@@ -6,6 +6,44 @@ import time
 
 app = Flask(__name__)
 
+#Must Run Commit After Function is called
+def addPost (title, post, time, pID, curs):
+
+    sinsertion = '''INSERT INTO posts (title, post, time, pId) 
+    values ("''' + title + '''", "''' + post+ '''", ''' + str(time) + ''', ''' + str(pID) + '''); '''
+
+    curs.execute(sinsertion)
+
+def trending(a):
+    one = 0
+    oneCount = 0
+    two = 0
+    twoCount = 0
+    three = 0
+    threeCount = 0
+    for post in a:
+        count = 0
+        for time in post:
+            if time - time.time() <= 86400:
+                count += 1
+        if count > oneCount:
+            three = two
+            threeCount = twoCount
+            two = one
+            twoCount = oneCount 
+            one = post[0]
+            oneCount = count
+        elif count >= twoCount:
+            three = two
+            threeCount = twoCount
+            two = post[0]
+            twoCount = count
+        elif count >= threeCount:
+            three = post[0]
+            threeCount = count
+    return [one,two,three]
+
+
 
 @app.route("/", methods = ['GET'])
 def index():
@@ -19,19 +57,16 @@ def index():
     c.execute('''CREATE TABLE IF NOT EXISTS comments
     (comment text, time real, pId integer, cId integer);''')
     
-    sinsertion = '''INSERT INTO posts (title, post, time, pId) 
-    values ('title', 'post', 12, 12);
-    ''' 
-    c.execute(sinsertion)    
+    addPost("post", "title", 18.781, 199, c)
 
     sdata = ''
 
     for row in c.execute('SELECT * FROM posts'):
         sdata += str(row)
-        
+
     conn.commit()
     conn.close()
-    
+    print sdata
     
     return render_template("index.html", sinput = sdata);
     
