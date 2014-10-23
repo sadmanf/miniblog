f92cbf582957f01976fa6fb843190e9952b2a190
Sadman Fahmid <sadmanfahmid@gmail.com>
2014-10-22 22:47:31 -0400

    can create new posts now

--- flaskr/app.py
+++ flaskr/app.py
@@ -6,7 +6,7 @@ import time
 
 app = Flask(__name__)
 
-posts = [("post1", "this is the first post HUrray fjawo;eij"), ("post2", "a;sodifjawo;eifja;woeifjaw;oeifjas;ldkfjha;skdjfhalskdjfhalskdjfhaskldjf")]
+#posts = [("post1", "this is the first post HUrray fjawo;eij"), ("post2", "a;sodifjawo;eifja;woeifjaw;oeifjas;ldkfjha;skdjfhalskdjfhalskdjfhaskldjf")]
 
 #Must Run Commit After Function is called
 def addPost (postdata, txtpostdata, time, curs):
@@ -63,8 +63,26 @@ def index():
     c.execute('''CREATE TABLE IF NOT EXISTS comments
     (comment text, time real, pId integer);''')
     #Insert code here to fetch all of the posts and set posts to that. - genji
+    
+    
+    c.execute("select * from posts")
+    #print [str(x[0]) for x in c.fetchall()]
+    titles = []
+    posts = []
+    for x in c.fetchall():
+        posts.append(x) 
+
+ #       print(str(x[0]))
+  #      print(str(x[1]))
+
+        
+    posts = posts[::-1]
+    print titles
+    # posts = posts[::-1]
+    #length = len(posts)
+#    print length
     #Insert code here to fetch the trending posts now. -genji
-    return render_template("index.html",location="Home",posts=posts,trending=trending)
+    return render_template("index.html", location="Home",posts=posts, trending=posts)
 
 @app.route("/new-post", methods=['POST'])
 def newPost():
@@ -78,9 +96,10 @@ def newPost():
 
         addPost(name, content, time.time(), c)
         #INSERT CODE HERE TO ADD A NEW POST. -genji
-        getPosts(c)
-        conn.commit()
 
+        getPosts(c)
+            
+        conn.commit()
         conn.close()
 
     return redirect("http://localhost:5000")
@@ -90,33 +109,13 @@ def post(post_id=None):
     print "Post ID: " + post_id
     return render_template("post.html",location=posts[int(post_id)][0])
 
-"""
-@app.route("/", methods = ['GET'])
-def index():
-    conn = sqlite3.connect('softblog.db')
-    c = conn.cursor()
-
-    postdata = request.args.get("post");
-    txtpostdata = request.args.get("txtpost");
-
-    c.execute('''CREATE TABLE IF NOT EXISTS posts(title text, post text, time integer, pId integer);''')
-    c.execute('''CREATE TABLE IF NOT EXISTS comments
-    (comment text, time real, pId integer, cId integer);''')
-
-    addPost(postdata, txtpostdata, 18.781, 199, c)
-
-    sdata = ''
-
-    for row in c.execute('SELECT * FROM posts'):
-        sdata += str(row)
-
-    conn.commit()
-    conn.close()
-
-    posts = [("post1", "this is the first post HUrray fjawo;eij"), ("post2", "a;sodifjawo;eifja;woeifjaw;oeifjas;ldkfjha;skdjfhalskdjfhalskdjfhaskldjf")]
-
-    return render_template("index.html", posts = posts );
-"""
+@app.route("/<post_id>/new-comment")
+def addComment(post_id=None):
+    #INSERT CODE HERE TO ADD A NEW COMMENT TO THE POST -genji
+    if request.method=='POST':
+        username = request.form('username')
+        content = request.form('comment')
+    return redirect("http://localhost:5000/"+post_id)
 
 
 if __name__ == "__main__":
